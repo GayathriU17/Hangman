2eedf9dd1c2ae157b6ff6a5468328f59550e3168
Gayathri U <gayathriu@Gayathris-MacBook-Air.local>
2022-08-03 21:02:57 +0530

    second commit

--- basic hangman.py
+++ basic hangman.py
@@ -1,9 +1,7 @@
-#Step 1 
 import random
 import string
-word_list = ["aardvark", "baboon", "camel", "apple", "movie"]
+word_list = ["chakkara", "baby", "camel", "momo", "sweetie"]
 chosen_word=random.choice(word_list)
-print(chosen_word)
 
 list_chosen_word=[]
 list_chosen_word=[char for char in chosen_word]
@@ -11,33 +9,30 @@ list_chosen_word=[char for char in chosen_word]
 
 for i in chosen_word:
   print('_', end='')
-string_of_bs='_'*(len(list_chosen_word))
+string_of_bs=('_'+'')*(len(list_chosen_word))
 list_of_bs=[char for char in string_of_bs]
 #print(list_of_bs)
 
-counter=len(list_chosen_word)
+counter=5
 
 while(counter>0):
-  guess_letter=input('\nGuess a letter')
-  counter-=1
+  guess_letter=input('\nGuess a letter\t')
+
   if (guess_letter in list_chosen_word):
     for i in range(len(list_chosen_word)):
       if(list_chosen_word[i]==guess_letter):
+        
         list_of_bs[i]=guess_letter
-
-print(list_of_bs)
-if(list_of_bs==list_chosen_word):
-  print(chosen_word)
-  
-  
-
-
-
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
-
+    print(list_of_bs)
+   
+    if(list_of_bs==list_chosen_word):
+      print(chosen_word)
+      print('CONGRATS! You WIN!!')
+      break
+    else:
+      continue
+    
+  else:
+    print(list_of_bs)
+    counter-=1
+    print(f"You lost a life. You now have {counter} chances left.")
