a4813bd08934719c0f593dfbda4e616fbceac19b
Gayathri U <gayathriu@Gayathris-MacBook-Air.local>
2022-08-03 22:42:22 +0530

    fifth commit

--- basic hangman.py
+++ basic hangman.py
@@ -1,5 +1,6 @@
 import random
 import string
+import hangman_art
 word_list = ["dog", "animal", "cake", "chocolate", "pencil"]
 chosen_word=random.choice(word_list)
 
@@ -13,7 +14,10 @@ string_of_bs=('_'+'')*(len(list_chosen_word))
 list_of_bs=[char for char in string_of_bs]
 #print(list_of_bs)
 
-counter=5
+counter=6
+
+print(hangman_art.logo)
+print(hangman_art.stages[6-counter])
 
 while(counter>0):
   guess_letter=input('\nGuess a letter\t')
@@ -21,9 +25,9 @@ while(counter>0):
   if (guess_letter in list_chosen_word):
     for i in range(len(list_chosen_word)):
       if(list_chosen_word[i]==guess_letter):
-        
         list_of_bs[i]=guess_letter
     print(list_of_bs)
+    print(hangman_art.stages[6-counter])
    
     if(list_of_bs==list_chosen_word):
       print(chosen_word)
@@ -36,7 +40,7 @@ while(counter>0):
     print(list_of_bs)
     counter-=1
     print(f"You lost a life. You now have {counter} chances left.")
+    print(hangman_art.stages[6-counter])
     if (counter==0):
       print('You LOSE :(')
       print(f'The word was {chosen_word}')
-
